a70b22f98bc723376548460ed41d682f4413bf29
sgarciachavez <sgarciachavez@gmail.com>
2019-04-07 22:48:28 -0500

    Add a getImageUrl function to address the issue of missing thumbnail image

--- src/Book.js
+++ src/Book.js
@@ -3,11 +3,19 @@ import ChangeShelf from './ChangeShelf'
 
 class Book extends Component{
 
+  getImageUrl(){
+    const book = this.props.book
+    const noimage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyg8da3e037NhfpTWzj9CLbyyUhQmcglvrXd0kIsa4bjHAim3yA"
+    const url = book.imageLinks !== undefined ? book.imageLinks.thumbnail : noimage
+    return `url("${url}")`
+
+  }
   render(){
       const {book,id,refreshShelves} = this.props
-      let thumbnail = `url("${book.imageLinks.thumbnail}")`
-      let noimage = `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyg8da3e037NhfpTWzj9CLbyyUhQmcglvrXd0kIsa4bjHAim3yA")`
-      thumbnail = thumbnail ? thumbnail : noimage
+      //let thumbnail = `url("${book.imageLinks.thumbnail}")`
+      const thumbnail = this.getImageUrl()
+
+      console.log(JSON.stringify(book.imageLinks))
       let authors = book.authors ? book.authors.join(', ') : "Author Unknown"
 
     return(
